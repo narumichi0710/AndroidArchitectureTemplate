b877737d1d3b66fc876273fbaac4d747a8428405
内田正輝 <masaki.u@arsaga.jp>
2021-07-17 23:46:35 +0900

    モジュール構造を組むための要素部品を定義

--- buildSrc/src/main/kotlin/ModuleStructure.kt
+++ buildSrc/src/main/kotlin/ModuleStructure.kt
@@ -1,5 +1,18 @@
 object ModuleStructure {
 
+    const val coreModulePathPostfix = "core"
+
+    internal enum class DomainType {
+        auth
+    }
+
+    internal enum class LayerType {
+        _presentation_viewModel_,
+        _domain_service_,
+        _domain_entity_,
+    }
+}
+
 fun Project.moduleStructure() {
     afterEvaluate {
     }
