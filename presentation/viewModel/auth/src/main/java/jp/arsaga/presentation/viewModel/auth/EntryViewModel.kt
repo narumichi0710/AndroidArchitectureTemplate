b1b5db2a63d4e7b9105d1b9ebdda0de750e766ed
内田正輝 <masaki.u@arsaga.jp>
2021-07-19 02:36:49 +0900

    ログイン/新規登録に関するViewModelのサンプル実装を作成

--- presentation/viewModel/auth/src/main/java/jp/arsaga/presentation/viewModel/auth/EntryViewModel.kt
+++ presentation/viewModel/auth/src/main/java/jp/arsaga/presentation/viewModel/auth/EntryViewModel.kt
@@ -1,17 +1,21 @@
 package jp.arsaga.presentation.viewModel.auth
 
 import android.app.Application
-import jp.arsaga.dataStore.repository.auth.EntryRepository
+import androidx.lifecycle.viewModelScope
+import jp.arsaga.dataStore.repository.auth.EntryCommandImpl
+import jp.arsaga.dataStore.repository.auth.EntryQueryImpl
 import jp.arsaga.domain.service.auth.EntryService
-import jp.arsaga.domain.service.core.BaseService
 import jp.arsaga.presentation.viewModel.core.extension.BaseViewModel
 
 class EntryViewModel(
-    application: Application,
-    dependency: EntryService.Dependency = EntryService.Dependency(
-        EntryRepository(), null
-    )
+    application: Application
 ) : BaseViewModel<EntryService.Dependency>(application) {
+
+    private val command = EntryCommandImpl(application, viewModelScope)
+
+    private val query = EntryQueryImpl(application, viewModelScope)
+
+    private val dependency = EntryService.Dependency(command, query)
 
     override val service = EntryService(dependency)
 }
