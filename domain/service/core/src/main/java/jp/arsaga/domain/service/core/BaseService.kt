cbb97ac3264d3b7a33e0ebd8198db165e2667907
内田正輝 <masaki.u@arsaga.jp>
2021-07-21 15:49:26 +0900

    Serviceのベースクラスにクラスドキュメント追加

--- domain/service/core/src/main/java/jp/arsaga/domain/service/core/BaseService.kt
+++ domain/service/core/src/main/java/jp/arsaga/domain/service/core/BaseService.kt
@@ -1,10 +1,37 @@
 package jp.arsaga.domain.service.core
 
+/**
+ * Serviceクラス(ロジックを表現するクラス)を共通化して使うための最低限のinterface
+ */
 interface BaseService<Dependency> {
+
     val dependency: Dependency
+
+    /**
+     * Serviceクラス内での外部依存情報のinterface
+     * 各プロパティにDTOをあてがって使用する
+     */
     interface Dependency {
+
+        /**
+         * 画面遷移させるラムダが並んだDTO interface
+         * 基本的な使われ方はcommandのCallback内で引数を受け取ってラムダを実行し、
+         * Viewのレイヤーで継承させたものをViewModelのコンストラクタ経由で受け取る
+         */
         val navigator: Any?
+
+        /**
+         * 外部データを操作するラムダが並んだDTO interface
+         * CQRSアーキテクチャパターンの「C」の責務
+         * 端末からサーバーやSaaSにデータを送る処理が並ぶ
+         */
         val command: Any?
+
+        /**
+         * 外からデータを受け取る型が並んだDTO interface
+         * CQRSアーキテクチャパターンの「Q」の責務
+         * FlowやLiveDataやObservableなどのReactiveプログラミングを実現する型が並ぶ
+         */
         val query: Any?
     }
 }
