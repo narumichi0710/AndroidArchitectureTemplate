6d46b6e57ac4a1a07a7ea509ca669f6b99063563
内田正輝 <masaki.u@arsaga.jp>
2021-08-10 05:44:04 +0900

    AuthFragmentをComposeを使った画面に変更

--- presentation/view/auth/src/main/java/jp/arsaga/presentation/view/auth/fragment/AuthFragment.kt
+++ presentation/view/auth/src/main/java/jp/arsaga/presentation/view/auth/fragment/AuthFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import jp.arsaga.domain.useCase.auth.AuthUseCase
 import jp.arsaga.domain.useCase.core.ActivityCallback
-import jp.arsaga.presentation.view.auth.databinding.FragmentLoginBinding
+import jp.arsaga.presentation.view.auth.R
+import jp.arsaga.presentation.view.core.extension.composable
 import jp.arsaga.presentation.viewModel.auth.AuthViewModel
 import jp.arsaga.presentation.viewModel.core.extension.BaseViewModel
-import jp.co.arsaga.extensions.view.bind
 import jp.co.arsaga.extensions.view.getNavController
 
 class AuthFragment : Fragment() {
@@ -36,8 +37,8 @@ class AuthFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = FragmentLoginBinding.inflate(inflater, container, false)
-        .bind(viewLifecycleOwner) {
-            it.viewModel = viewModel
+    ): View = composable(R.id.loginFragment) {
+            Text (text = "Hello Masaki!")
         }
+    }
 }
