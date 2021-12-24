320c8f6476a1057674f967908583c9a015ae95bc
内田正輝 <masaki.u@arsaga.jp>
2021-12-24 20:48:46 +0900

    ディープリンク起動を受け取ってハンドリングするActivityを作成

---
+++ app/src/main/java/jp/arsaga/app/activity/DeepLinkActivity.kt
@@ -0,0 +1,39 @@
+package jp.arsaga.app.activity
+
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.arsaga.app.App
+import jp.arsaga.presentation.view.core.navigator.DeepLinkResolver
+
+class DeepLinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DeepLinkResolver.setDeepLink(intent?.data)
+        App.persistentContainer.add(DeepLinkResolver)
+        executeDeepLink()
+    }
+
+    private fun executeDeepLink() {
+        if (isAuth()) {
+            Intent(this, AuthActivity::class.java)
+                .setFlags(FLAG_ACTIVITY_SINGLE_TOP)
+        } else {
+            Intent(this, MainActivity::class.java)
+                .setFlags(FLAG_ACTIVITY_CLEAR_TOP)
+        }.let {
+            startActivity(it, null)
+        }
+    }
+
+    private fun isAuth(): Boolean = true
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
+
